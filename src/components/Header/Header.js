46b12ef92c50d3b804ec654732ad00e4116dc4a5
ZacharyDonovan88 <zachary.donovan88@outlook.com>
2023-02-06 18:56:16 +1030

    update, added header/style

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return (
+        <head>
+            <title>Zachary's Portfolio</title>
+        </head>
+    );
+  }
+  
+  export default Header;
+  
