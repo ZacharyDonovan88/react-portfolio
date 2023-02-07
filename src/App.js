151d369c5d2025eb97ec0b6569992c45223cf5ec
ZacharyDonovan88 <zachary.donovan88@outlook.com>
2023-02-07 19:42:41 +1030

    added Work to app

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import Header from "./components/Header/Header.js";
 import Home from "./components/Home/Home.js";
 import Navbar from "./components/Navbar/Navbar.js";
 import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
-
+import About from "./components/About/About.js";
+import Work from "./components/Work/Work.js"
 
 function App() {
   return (
     <div>
       <Home />,
       <Navbar />
+      <div id="sidebar-padding">
+        <About />
+        <Work />
+      </div>
     </div>
   );
 }
