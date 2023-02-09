2db2648862241e4dda11e7a596bce98ff4431e5f
ZacharyDonovan88 <zachary.donovan88@outlook.com>
2023-02-09 21:51:14 +1030

    update

--- src/components/ContactMail/ContactMail.js
+++ src/components/ContactMail/ContactMail.js
@@ -17,21 +17,6 @@ const USER_ID = "XEPEOvc2cWZKV_VfM";
 const App = () => {
   const handleOnSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
-      .then((result) => {
-        console.log(result.text);
-        Swal.fire({
-          icon: 'success',
-          title: 'Message Sent Successfully'
-        })
-      }, (error) => {
-        console.log(error.text);
-        Swal.fire({
-          icon: 'error',
-          title: 'Ooops, something went wrong',
-          text: error.text,
-        })
-      });
     e.target.reset()
   };
 
